91eec3105abd775039f14e2ecadd1777453fb4f4
Lakshmeesh <lakshmishmanavi@gmail.com>
2024-09-25 23:05:55 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 function guessinggame()
 {
-nfiles=$(ls | wc -l)
-while [[ 1 -eq 1 ]]
-do
-read -p "Enter the number files in current directory: " number
-echo $number
-if [[ $number -eq $nfiles ]];
-then
-echo "Congratulations"
-exit
-elif [[ $number -gt $nfiles ]];
-then
-echo "Number is larger"
-else
-echo "Smaller"
-fi
-echo "Try again"
+# Generate a random number between 1 and 100
+secret_number=$((RANDOM % 100 + 1))
+guess=0
+attempts=0
+
+echo "Welcome to the Guessing Game!"
+echo "I'm thinking of a number between 1 and 100."
+
+# Loop until the user guesses the correct number
+while [ $guess -ne $secret_number ]; do
+    read -p "Enter your guess: " guess
+    attempts=$((attempts + 1))
+
+    # Validate that the input is a number
+    if ! [[ $guess =~ ^[0-9]+$ ]]; then
+        echo "Please enter a valid number."
+        continue
+    fi
+
+    if [ $guess -lt $secret_number ]; then
+        echo "Too low! Try again."
+    elif [ $guess -gt $secret_number ]; then
+        echo "Too high! Try again."
+    else
+        echo "Congratulations! You've guessed the number $secret_number in $attempts attempts."
+    fi
 done
 }
 guessinggame
